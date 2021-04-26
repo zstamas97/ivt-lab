4fa486d32de97b2709e64b12283c6bd6fa3a5107
zstamas97 <zs.tamas97@gmail.com>
2021-04-26 16:16:26 +0200

    FixRandom

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.spaceship;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -13,6 +15,8 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
+
+  
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
@@ -28,7 +32,7 @@ public class TorpedoStore {
     }
   }
 
-  public boolean fire(int numberOfTorpedos){
+  public boolean fire(int numberOfTorpedos) throws NoSuchAlgorithmException{
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
     throw new IllegalArgumentException("numberOfTorpedos");
     }
@@ -36,8 +40,8 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
-    double r = generator.nextDouble();
+    Random rand = SecureRandom.getInstanceStrong();
+    double r = rand.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
